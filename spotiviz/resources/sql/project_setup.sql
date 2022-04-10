5d19e5ebe10c3b0e81b2d7da7cf44d50f47858d3
kwilinsi <kwilinsi.314@gmail.com>
2022-04-09 22:41:05 -0400

    Add documentation to project_setup.sql

--- spotiviz/resources/sql/project_setup.sql
+++ spotiviz/resources/sql/project_setup.sql
@@ -1,6 +1,6 @@
 /*
- * These SQL instructions set up the sqlite database for a specific project. This is
- * executed whenever a new project is created.
+ * These SQL instructions set up the sqlite database for a specific project.
+ * This is executed whenever a new project is created.
  */
 
 DROP TABLE IF EXISTS Downloads;
@@ -8,6 +8,12 @@ DROP TABLE IF EXISTS StreamingHistories;
 DROP TABLE IF EXISTS StreamingHistoryRaw;
 DROP TABLE IF EXISTS StreamingHistory;
 
+/*
+ * The Downloads table contains a list of Spotify downloads.
+ *
+ * Each download has a unique, auto-incrementing ID; a path to the download's
+ * folder; a name; and a start date.
+ */
 CREATE TABLE Downloads
 (
     id         INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,6 +22,15 @@ CREATE TABLE Downloads
     start_time TIMESTAMP
 );
 
+/*
+ * The StreamingHistories table contains a list of streaming history files.
+ *
+ * Each streaming history file is associated with a specific download,
+ * specified by its id. The file is given only by its name (e.g.
+ * StreamingHistory0.json), which is appended to the download's path to get
+ * the full path to the history file. This also includes a timestamp for the
+ * the first listen in the streaming history.
+ */
 CREATE TABLE StreamingHistories
 (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,6 +41,14 @@ CREATE TABLE StreamingHistories
     UNIQUE (download_id, file_name)
 );
 
+/*
+ * The StreamingHistoryRaw table contains all the contents of the streaming
+ * history files given in the StreamingHistories table.
+ *
+ * Each StreamingHistory#.json file contains a list of Spotify listen records.
+ * When the files are parsed, each of those listens is stored in this table.
+ * Note however that this table will likely contain duplicate entries.
+ */
 CREATE TABLE StreamingHistoryRaw
 (
     history_id  INTEGER,
@@ -38,6 +61,14 @@ CREATE TABLE StreamingHistoryRaw
     FOREIGN KEY (history_id) REFERENCES StreamingHistories (id)
 );
 
+/*
+ * The StreamingHistory table is a duplicate of the StreamingHistoryRaw table
+ * where the duplicate listens have been removed.
+ *
+ * Each listen includes a Spotify track name, the artist name, the number of
+ * milliseconds spent listening to that song, and the end time that the song
+ * stopped playing.
+ */
 CREATE TABLE StreamingHistory
 (
     position    INTEGER,
