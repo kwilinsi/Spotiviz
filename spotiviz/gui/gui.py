f744e7a0fcadea0b20515435f2a4a93d7c28c329
kwilinsi <kwilinsi.314@gmail.com>
2022-04-15 14:19:59 -0400

    Create initial home screen window

---
+++ spotiviz/gui/gui.py
@@ -0,0 +1,24 @@
+from PyQt6.QtWidgets import QApplication
+
+from spotiviz.gui import homecreen
+
+APP = None
+HOME = None
+
+def start() -> None:
+    """
+    Start the main GUI, initializing the Qt application and displaying the
+    home screen.
+
+    Returns:
+        None
+    """
+
+    global APP, HOME
+
+    APP = QApplication([])
+
+    HOME = homecreen.HomeScreen()
+    HOME.show()
+
+    APP.exec()
---
+++ spotiviz/gui/homecreen.py
@@ -0,0 +1,6 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class HomeScreen(QMainWindow):
+    def __init__(self):
+        super().__init__()
