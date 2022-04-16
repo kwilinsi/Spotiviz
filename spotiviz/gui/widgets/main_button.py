2b742a871b343c7c8160739f82cd08b331afa6af
kwilinsi <kwilinsi.314@gmail.com>
2022-04-15 21:27:40 -0400

    Increase main button minimum width

--- spotiviz/gui/widgets/main_button.py
+++ spotiviz/gui/widgets/main_button.py
@@ -5,7 +5,7 @@ class MainButton(QPushButton):
     def __init__(self, text: str):
         super().__init__(text)
 
-        self.setMinimumSize(100, 40)
-        self.setMaximumSize(150, 40)
+        self.setMinimumSize(130, 40)
+        self.setMaximumSize(175, 40)
         self.setSizePolicy(QSizePolicy.Policy.Preferred,
                            QSizePolicy.Policy.Expanding)
