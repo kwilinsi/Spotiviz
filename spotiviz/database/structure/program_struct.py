97f40967e3fc461baad08dcd591439579ed6a2f4
kwilinsi <kwilinsi.314@gmail.com>
2022-05-22 23:16:47 -0400

    [WIP] Switch program_struct.py to SQLite specific SQLAlchemy data types

--- spotiviz/database/structure/program_struct.py
+++ spotiviz/database/structure/program_struct.py
@@ -3,7 +3,8 @@ This models the SQLite database structure for the main Spotiviz program-level
 database.
 """
 
-from sqlalchemy import Column, Text, DateTime
+from sqlalchemy import Column
+from sqlalchemy.dialects.sqlite import TEXT, DATETIME
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql.functions import func
 
@@ -14,9 +15,10 @@ Base = declarative_base()
 
 class Projects(Base, ReprModel):
     __tablename__ = "Projects"
-    name: Column = Column(Text, primary_key=True)
-    database_path: Column = Column(Text, nullable=False)
-    created_at: Column = Column(DateTime, nullable=False, server_default=func.now())
+    name: Column = Column(TEXT, primary_key=True)
+    database_path: Column = Column(TEXT, nullable=False)
+    created_at: Column = Column(DATETIME, nullable=False,
+                                server_default=func.now())
 
     def __repr__(self) -> str:
         return self._repr(name=self.name,
