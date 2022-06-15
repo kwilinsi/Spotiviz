a7c4767b320fd5eea451e0659825551c4350e942
kwilinsi <kwilinsi.314@gmail.com>
2022-06-15 09:23:39 -0400

    Make preferences dialog save config to database

--- spotiviz/gui/windows/preferences.py
+++ spotiviz/gui/windows/preferences.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import List
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
@@ -6,6 +6,10 @@ from PyQt6.QtWidgets import (
     QLineEdit, QPushButton, QDialogButtonBox, QMainWindow, QWidget,
     QLayout, QScrollArea, QMessageBox
 )
+
+from sqlalchemy import update
+
+from spotiviz.database.structure.project_struct import Config as ConfigTbl
 
 from spotiviz.projects.structure.project_class import Project
 from spotiviz.projects.structure.config.properties import Config
@@ -34,6 +38,7 @@ class Preferences(QDialog):
         super().__init__(parent)
 
         self.project = project
+        self.fields: List[ConfigField] = []
 
         self.setWindowTitle('Preferences')
 
@@ -52,13 +57,14 @@ class Preferences(QDialog):
         self.tabs.addTab(self.aliases, 'Aliases')
 
         # Add buttons to this dialog
-        buttons = QDialogButtonBox(
+        self.buttons = QDialogButtonBox(
+            QDialogButtonBox.StandardButton.Apply |
             QDialogButtonBox.StandardButton.Ok |
             QDialogButtonBox.StandardButton.Cancel
         )
-        buttons.accepted.connect(self.accept)
-        buttons.rejected.connect(self.reject)
-        layout.addWidget(buttons)
+
+        self.buttons.clicked.connect(self.btn_clicked)
+        layout.addWidget(self.buttons)
 
     def _get_config_tab(self) -> QWidget:
         """
@@ -80,10 +86,13 @@ class Preferences(QDialog):
         form = QFormLayout()
         layout.addLayout(form)
 
+        # Forcibly reload the project config from the database
+        self.project.config.read_from_db()
+
         # Add form entries
         for prop in self.project.config.properties:
-            lbl, h_lyt = self.get_form_property(prop)
-            form.addRow(lbl, h_lyt)
+            self.fields.append(ConfigField(prop, self, self.project))
+            form.addRow(QLabel(prop.value.friendly_name), self.fields[-1])
 
         return layout
 
@@ -109,41 +118,98 @@ class Preferences(QDialog):
 
         return layout
 
-    def get_form_property(self, config: Config) -> Tuple[QLabel, QHBoxLayout]:
+    def btn_clicked(self, btn) -> None:
         """
-        Take some configurable property and construct a label and layout for it
-        that can be added to the configuration form layout.
-
-        Args:
-            config: The configurable property to add to the form.
+        This function is called when the user clicks either one of the
+        buttons at the bottom of the dialog: 'Apply', 'Ok', or 'Cancel'.
 
         Returns:
-            A label for the property, and a layout containing a field and
-            button
-            for getting more info on the button.
+            None
         """
 
-        # Create label
-        lbl = QLabel(config.value.friendly_name)
+        role = self.buttons.buttonRole(btn)
+
+        if role == QDialogButtonBox.ButtonRole.AcceptRole or \
+                role == QDialogButtonBox.ButtonRole.ApplyRole:
+
+            # Identify the fields that changed
+            changed = [f for f in self.fields if f.has_changed]
+
+            # If any fields changed, update them
+            if changed:
+                with self.project.open_session() as session:
+                    for field in changed:
+                        session.execute(
+                            update(ConfigTbl).
+                                where(ConfigTbl.key == field.config.name).
+                                values(value=field.current_value)
+                        )
+                        field.on_apply()
+                    session.commit()
+
+                # Forcibly reload the project config from the database
+                self.project.config.read_from_db()
+
+        # If 'Ok' or 'Cancel' were clicked, close the dialog
+        if role == QDialogButtonBox.ButtonRole.AcceptRole or \
+                role == QDialogButtonBox.ButtonRole.RejectRole:
+            self.close()
+
+
+class ConfigField(QHBoxLayout):
+    """
+    This class represents each of the fields found in the configuration
+    section of the preferences dialog. Fields are typically a QLineEdit
+    widget,
+    but for different data types they may be different widgets.
+
+    Next to each field is also a button that provides more information on
+    the
+    meaning of the property.
+    """
+
+    def __init__(self,
+                 config: Config,
+                 preferences_dialog: Preferences,
+                 project: Project):
+        """
+        Create a new field by providing the config property that it
+        represents.
+
+        Args:
+            config: The config property.
+            preferences_dialog: The preferences dialog instance to which
+            this
+                                field belongs.
+            project: The project the user is working on.
+        """
+
+        super().__init__()
+
+        self.config: Config = config
+        self.preferences_dialog: Preferences = preferences_dialog
+
+        self.has_changed: bool = False
+        self.initial_value = project.config.properties[config]
+        self.current_value = self.initial_value
 
         # Create input field
-        h_lyt = QHBoxLayout()
-        field = QLineEdit()
-        h_lyt.addWidget(field)
+        self.field = QLineEdit()
+        self.field.textEdited.connect(self.edited)
+        self.addWidget(self.field)
 
         # Set the current value
-        field.setText(str(self.project.config.properties[config]))
+        self.field.setText(str(self.initial_value))
 
         # Create an info button
         info_btn = InfoBtn(lambda: self.info_btn(config))
-        h_lyt.addWidget(info_btn)
-
-        return lbl, h_lyt
+        self.addWidget(info_btn)
 
     def info_btn(self, config: Config) -> None:
         """
         This function is called when a user clicks one of the info buttons
-        next to a config property. It opens a small message box with detailed
+        next to a config property. It opens a small message box with
+        detailed
         information on what the property controls.
 
         Args:
@@ -153,10 +219,36 @@ class Preferences(QDialog):
             None
         """
 
-        msg = QMessageBox(self)
+        msg = QMessageBox(self.preferences_dialog)
         msg.setWindowTitle(f'{config.value.friendly_name} - Info')
         msg.setText(config.value.description)
         msg.exec()
+
+    def edited(self) -> None:
+        """
+        This is called whenever the user changes the value in this field. It
+        checks to see if the new value is different from the starting value,
+        and if so, it updates self.has_changed.
+
+        Returns:
+            None
+        """
+
+        self.current_value = self.field.text()
+        self.has_changed = self.current_value != self.initial_value
+
+    def on_apply(self) -> None:
+        """
+        This function is called when the user clicks the 'Apply' or 'Ok'
+        buttons. Since self.current_value has now been pushed to the
+        database, self.initial_value is reset to that value.
+
+        Returns:
+            None
+        """
+
+        self.initial_value = self.current_value
+        self.has_changed = False
 
 
 def make_scrollable(layout: QLayout) -> QScrollArea:
